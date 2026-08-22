bca7d04e7d2af54e0282e7c93496f22b3ea6af2b
suraueristana <bendahari.suraueristana@gmail.com>
2026-08-22 12:29:16 +0800

    Add asset and inventory management module

---
+++ asset-config.js
@@ -0,0 +1,6 @@
+// Konfigurasi Supabase untuk modul aset
+window.ASET_CONFIG = {
+  SUPABASE_URL: 'https://zhqpduobgnooitbiysgc.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_IqVhiEoboYeaEvFy0Yfgeg__a4dewJm',
+  PUBLIC_ASSET_URL: 'https://suraueristana.github.io/main/asset-public.html'
+};
